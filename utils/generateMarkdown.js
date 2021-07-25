c4a7f2cdbdd87fc9f7750fcdaaebf5c1fb1c2644
ncougs <ncougan@gmail.com>
2021-07-25 13:34:56 +1000

    adding renderLicenseBadge & renderLicenseLink functions

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,24 +1,48 @@
 const fs = require('fs');
 const moment = require('moment');
 
+//Function to read the file containing the license text. 
 const readLicense = (fileName) => {
   return fs.readFileSync(`./utils/${fileName}`).toString()
 };
 
-// TODO: Create a function that returns a license badge based on which license is passed in
+// function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (license == null) {
+    return;
+  }
+  else {
+    return `[![License]`;
+  };
+};
 
-// TODO: Create a function that returns the license link
+// function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (license == null) {
+    return;
+  };
 
-// TODO: Create a function that returns the license section of README
+  if (license=='MIT License') {
+    return `(https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)`;
+  };
+
+  if (license=='GNU General Public License') {
+    return `(https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)`;
+  };
+
+  if (license=='Apache License') {
+    return `(https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)`;
+  };
+}
+
+// Function that returns the license section of README
 // If there is no license, return an empty string
 const renderLicenseSection = (license, name) => {
   let licenseString;
 
-  if (license == 'null') {
+  if (license == null) {
     return;
   };
 
@@ -42,7 +66,9 @@ function generateMarkdown(data) {
 
 const title = data.title.split(' ').join('-').toLowerCase();
 
-return `# ${title}
+return `${renderLicenseBadge(data.license)}${renderLicenseLink(data.license)}
+
+# ${title}
   
 ## Description
   
